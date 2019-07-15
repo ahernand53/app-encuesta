52ee4932c2cf22186ab2ff87e22b3618b52b6bda
Asdrubal Hernandez <ahernand.development@gmail.com>
2019-07-15 15:16:40 -0500

    Agregando componententes de vue en administration site

---
+++ resources/js/components/Admins/Polls/StagesListComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <table class="table text-center">
+            <thead>
+            <th>#</th>
+            <th >Titulo</th>
+            <th >Descripcion</th>
+            <th>acciones</th>
+            </thead>
+
+            <tbody>
+                <template v-for="stage in stages">
+                    <stage-component :stage="stage" :key="stage.id"></stage-component>
+                </template>
+            </tbody>
+
+        </table>
+
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    import StageComponent from './StageComponent.vue'
+
+    export default {
+        name: "stages-component",
+        data() {
+            return {
+                stages: [],
+            }
+        },
+        methods: {
+            getUrlEdit(id) {
+                return 'edit/' . id
+            }
+        },
+        mounted() {
+            axios.get('stages').then(response => this.stages = response.data);
+        },
+        components: {
+            'stage-component': StageComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/Administration/Poll/stages/index.blade.php
+++ resources/views/Administration/Poll/stages/index.blade.php
@@ -16,58 +16,7 @@
                 <a href="/dashboard/formulario/stages/create" class="btn btn-success rounded rounded-circle"><i class="fas fa-plus text-dark"></i></a>
             </div>
         </div>
-        <table class="table text-center">
-            <thead>
-            <th>#</th>
-            <th >Titulo</th>
-            <th >Descripcion</th>
-            <th>acciones</th>
-            </thead>
-
-            <tbody>
-
-            @foreach($stages as $stage)
-                <tr >
-                    <td>{{ $stage->id }}</td>
-                    <td>{{ $stage->title }}</td>
-                    <td>{{ $stage->description }}</td>
-                    <td>
-                        <a href="/dashboard/formulario/stages/{{$stage->id}}/edit" class="btn btn-secondary">Editar</a>
-                        &nbsp;
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal{{ $stage->id }}">
-                            Eliminar
-                        </button>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="deleteModal{{ $stage->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <form method="post" action="{{ route('stages.destroy', [$stage->id]) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Eliminar Etapa</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Desea eliminar la estapa?
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                            <input type="submit" class="btn btn-primary" value="Eliminar">
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-
-        </table>
+        <stages-list-component></stages-list-component>
     </div>
 
 
